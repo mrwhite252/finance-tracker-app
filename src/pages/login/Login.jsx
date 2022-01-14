f93ce53b253d3f6dfd7ffcd0c6ebe5ac8505de90
DavidBai <baiminghan@gmail.com>
2022-01-14 14:09:05 +1300

    binding the data input for the login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+
 import styles from "./Login.module.css";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
-    <form className={styles["login-form"]}>
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
       <h2>Log in</h2>
       <label>
         <span>Email:</span>
-        <input type="email" />
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
       </label>
 
       <label>
         <span>Password:</span>
-        <input type="password" />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
       </label>
 
       <button className="btn">Log In</button>
