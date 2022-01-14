d452f26cb74d32329e91e689dac609eed1a6b6bc
DavidBai <baiminghan@gmail.com>
2022-01-14 15:52:21 +1300

    init firebase & firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHJ5JHaW9490_ZwgXw1ijx_WJOyYzSSy0",
+  authDomain: "financetracker-da543.firebaseapp.com",
+  projectId: "financetracker-da543",
+  storageBucket: "financetracker-da543.appspot.com",
+  messagingSenderId: "296968713509",
+  appId: "1:296968713509:web:456f2095c37db020a656cc",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
