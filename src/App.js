d7082638c9ec2f051005a6a825863b795f091edf
DavidBai <baiminghan@gmail.com>
2022-01-14 13:32:19 +1300

    add navbar components and style it

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
