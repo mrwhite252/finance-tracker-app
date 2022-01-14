1d9f8fb7eda8c6cea534fc3ddb2cd187a4bf51af
DavidBai <baiminghan@gmail.com>
2022-01-14 14:02:56 +1300

    setup login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,20 @@
-import "./Login.module.css";
+import styles from "./Login.module.css";
 
 export default function Login() {
-  return <div>login</div>;
+  return (
+    <form className={styles["login-form"]}>
+      <h2>Log in</h2>
+      <label>
+        <span>Email:</span>
+        <input type="email" />
+      </label>
+
+      <label>
+        <span>Password:</span>
+        <input type="password" />
+      </label>
+
+      <button className="btn">Log In</button>
+    </form>
+  );
 }
