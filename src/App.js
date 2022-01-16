6474004d4fda193a6134711e0dbea379475f6086
DavidBai <baiminghan@gmail.com>
2022-01-16 21:33:48 +1300

    add the authIsReady state to the app in order to fix the bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { useAuthContext } from "./hooks/useAuthContext";
+
 import Navbar from "./components/Navbar/Navbar";
 
 import Home from "./pages/home/Home";
@@ -6,16 +9,19 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 
 function App() {
+  const { authIsReady } = useAuthContext();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
