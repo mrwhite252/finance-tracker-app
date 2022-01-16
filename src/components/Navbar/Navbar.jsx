026963cb0c027ce1bb2176d25b38e8d19d0d551c
DavidBai <baiminghan@gmail.com>
2022-01-16 21:15:34 +1300

    use the use auth hook in the navbar to conditionally rendering the content

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,42 @@
 import { Link } from "react-router-dom";
 
+// custom hooks
 import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 // styles
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>My Money</li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
