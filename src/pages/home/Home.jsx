bd2ae3efa6b59abf7e915246977b720e8034ec92
DavidBai <baiminghan@gmail.com>
2022-01-23 21:14:59 +1300

    set up the add transaction form and style it up

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,13 @@
-import "./Home.module.css";
+import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
